eb6bc1582d5bf7b3e4efd9bd8d779788d4d8fa98
RonierisonBraz <44789415+RonierisonBraz@users.noreply.github.com>
2022-04-06 14:57:19 -0300

    terminando aula de comandos prompt JS

---
+++ Projetos/18_Math/scripts.js
@@ -0,0 +1,14 @@
+let maior = Math.max(6,12,28,55,8);
+
+console.log(`o numero maior é ${maior}`);
+
+let menor = Math.min(6,12,28,55,8);
+
+console.log(`o numero menor é ${menor}`);
+
+let arredondar = Math.round(6.56565656);
+
+console.log(`o numero arredondado é ${arredondar}`);
+let arredondarParaCima = Math.ceil(5.2934723);
+
+console.log(`o numero arredondado para mais ${arredondarParaCima}`);
---
+++ Projetos/19_consolelog/scripts.js
@@ -0,0 +1,6 @@
+let idade = 29;
+let nome = "ronierison"
+
+console.log(idade);
+console.log(nome);
+console.log(`O meu nome é ${nome}, e tenho ${idade} anos`);
