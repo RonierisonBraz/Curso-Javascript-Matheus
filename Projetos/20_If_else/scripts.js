836c97f9bbdf8dfd12ad65b6beda349530c150b7
RonierisonBraz <44789415+RonierisonBraz@users.noreply.github.com>
2022-08-13 23:06:07 -0300

    incluindo if e else_if

---
+++ Projetos/20_If_else/scripts.js
@@ -0,0 +1,31 @@
+let idade = 29;
+
+if(idade == 19)
+{
+    console.log("A idade é = 19");
+}
+
+if(idade > 25)
+{
+    console.log("A idade é maior que 25");
+}
+
+let nome = "Ronierison"
+let passaport  = true ; 
+
+if(nome == "Ronierison" && idade > 10)
+{
+    console.log("Liberado!");
+}
+
+if(nome == "Ronierison" && idade > 30 || passaport == true)
+{
+    console.log("Liberado!");
+}
+
+let velocidade = 100;
+if(velocidade < 80) {
+    console.log("não foi multado")
+}else{
+    console.log("foi mutado")
+}
