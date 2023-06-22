d64598751ee75ee65b0d7427cd6965e1502ef8f0
Ronierison Braz <ronierison18@gmail.com>
2023-06-22 00:57:23 -0300

    exercicio 20

---
+++ Exercicios/020/script.js
@@ -0,0 +1,14 @@
+function podeDigigir(idade) {
+    if (idade >= 18) {
+        return console.log("Pode dirigir");
+    } else {
+        return console.log("Não pode dirigir");
+    }
+
+    return Math.floor(Math.random() * a) + 1;
+    //Math.floor serve pra arredondar o numero do metodo random.
+}
+
+podeDigigir(20);
+podeDigigir(30);
+podeDigigir(16);
