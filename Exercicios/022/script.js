57eb02c7b7bff70c2f26a49bd5b690f81414eb55
Ronierison Braz <ronierison18@gmail.com>
2023-06-22 01:59:10 -0300

    exercicios 22 e 23 concluidos

---
+++ Exercicios/022/script.js
@@ -0,0 +1,9 @@
+function retornaPositivo(numb) {
+    return console.log(Math.abs(numb));
+    // return console.log(numb * (-1));
+}
+
+retornaPositivo(-30);
+retornaPositivo(-40);
+retornaPositivo(5);
+retornaPositivo(-50);
---
+++ Exercicios/023/script.js
@@ -0,0 +1,13 @@
+function contaCaracteres(palavra) {
+    if(palavra.length > 10){
+        console.log("texto muito longo");
+    }else {
+        console.log("texto dentro do limite");
+    }
+    
+}
+
+contaCaracteres("Ronierison Braz");
+contaCaracteres("teste");
+contaCaracteres("Thais");
+contaCaracteres("leticia");
