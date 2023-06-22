7aad02e2150189fb47c101aa449e4d1dc4124fb3
Ronierison Braz <ronierison18@gmail.com>
2023-06-22 02:27:36 -0300

    exercico 25 finalizado

---
+++ Exercicios/025/script.js
@@ -0,0 +1,10 @@
+function decrementaNumero(numb) {
+    for (let i = numb; i >= 0; i--) {
+        if (i % 2 == 0) {
+            console.log(i);
+        }
+        
+    }
+}
+
+decrementaNumero(100);
