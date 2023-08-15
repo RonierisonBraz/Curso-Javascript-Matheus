8f8837e64280e058aa72bfb6a51e8922c40dbcb9
Ronierison <ronierison.silva@k2m.com.br>
2023-08-15 09:11:20 -0300

    Arrumando as pastas

---
+++ Projetos/32_array/03_propriedades/scripts.js
@@ -0,0 +1,9 @@
+let nome = "joão";
+
+console.log(nome.length);//apos o ponto é uma propriedade que pode ser atribuida a variavel.
+
+let numeros = [1, 23, 34, 5, 6, 7, 8];
+
+console.log(numeros.length);
+console.log(numeros['length']);
+console.log(numeros[1]);
