cf62f67dc5f913e0cf88874aa707274daf7920d4
Ronierison <ronierison.silva@k2m.com.br>
2023-08-15 10:32:24 -0300

    Criando objetos

---
+++ Projetos/32_array/05_objetos/scripts.js
@@ -0,0 +1,14 @@
+let cachorro = {
+  patas: 4,
+  nome: 'shark',
+  latir: function () {
+    console.log("au au");
+  }
+};
+
+console.log(cachorro.patas);
+console.log(cachorro.nome);
+cachorro.latir();
+
+ //[] = array
+ // {} = objeto
