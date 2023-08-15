ea6b339ebc244b9ae5afda5e361b6ab466989e05
Ronierison <ronierison.silva@k2m.com.br>
2023-08-15 08:53:41 -0300

    iniciando nos array

---
+++ Projetos/33_maisSobreArray/scripts.js
@@ -0,0 +1,10 @@
+let arr = [1, 4, 5, 6, 7];
+let nomes = ['roni', 'Thais', 'laura', 'luisa'];
+let bool = [true, false, true];
+
+let misturado = [1, 'roni', true];
+
+console.log(arr[1]);
+console.log(nomes[0]);
+console.log(bool[2]);
+console.log(misturado[misturado.length - 1]);
