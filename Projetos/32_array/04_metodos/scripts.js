40bcdb77e16814743de14d36d7c31b2c6ba6e8e8
Ronierison <ronierison.silva@k2m.com.br>
2023-08-15 09:08:50 -0300

    Arrumando as pastas

---
+++ Projetos/32_array/04_metodos/scripts.js
@@ -0,0 +1,12 @@
+let nome = "thais";
+
+console.log(nome.toUpperCase());
+
+let marca2 = marca.toUpperCase();
+
+console.log(marca2.toLowerCase());
+
+console.log(typeof marca2.toLowerCase());
+
+//string.propriedades
+//string.metodo() tendo o parentese no metodo
