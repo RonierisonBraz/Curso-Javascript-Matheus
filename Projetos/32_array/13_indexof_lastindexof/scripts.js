f27b4dbddbcfa80013d0bc9ebe1bd415ca5ed786
Ronierison <ronierison.silva@k2m.com.br>
2023-08-18 17:48:20 -0300

    metodos indexOf e lastIndexOf

---
+++ Projetos/32_array/13_indexof_lastindexof/scripts.js
@@ -0,0 +1,9 @@
+let nums = [5, 6, 2, 4, 9, 6, 2];
+
+console.log(nums.indexOf(2));
+
+console.log(nums.lastIndexOf(6));
+
+
+//indexOf() procura primeiro index que foi passado por parametro
+//lastIndexOf() procura o ultimo index que foi passado por parametro
