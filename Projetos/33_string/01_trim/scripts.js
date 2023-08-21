418fb84712ecfb047cad2f67cc611a5f6c903246
RonierisonBraz <ronierison18@gmail.com>
2023-08-21 16:52:01 -0300

    metodos trim e padStart

---
+++ Projetos/33_string/01_trim/scripts.js
@@ -0,0 +1,9 @@
+let texto = "         Ronierison       "
+
+let nomeCorrigido = texto.trim();
+
+console.log(texto);
+console.log(nomeCorrigido);
+
+
+//trim retira todos os espaço em branco
---
+++ Projetos/33_string/02_padStart/scripts.js
@@ -0,0 +1,15 @@
+let sku = "34"
+
+console.log(sku.padStart(6, "0"));
+
+let sku2 = "7348"
+
+console.log(sku2.padStart(6, "0"));
+
+let sku3 = "Roni"
+
+console.log(sku3.padStart(6,"0"));
+
+
+//padStart serve para padronizar a quantidade de caracteres de uma string
+
