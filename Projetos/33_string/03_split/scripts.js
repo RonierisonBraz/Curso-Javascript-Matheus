9f974830f91cbdb59c6ff719d12ca72208fe8d80
RonierisonBraz <ronierison18@gmail.com>
2023-09-04 15:33:03 -0300

    metodo split

---
+++ Projetos/33_string/03_split/scripts.js
@@ -0,0 +1,14 @@
+let frase = "Testando o metodo split" ;
+
+console.log(frase.split(" "));
+
+let palavras = frase.split(" ");
+
+console.log(palavras);
+
+let produtos = "banana;maçã;jaca;pera;uva";
+
+console.log(produtos.split(";"));
+
+// split sever pra dividi uma string a parti de um determinado caracteres
+
